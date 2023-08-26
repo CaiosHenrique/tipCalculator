320ffc0acac6c68ca0565dac8d4e1c1d7fb0c07e
Caio <Caiokaikai@gmail.com>
2023-08-25 21:32:05 -0300

    second commit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,7 +4,21 @@ namespace tipCalculator;
 
 public partial class MainPage : ContentPage
 {
-	void CalculateTip(bool OnNormalTip ,bool OnGenarateTip, bool roundUp, bool roundDown)
+	public MainPage()
+	{
+		InitializeComponent();
+		billInput.TextChanged += (s, e) => CalculateTip(false, false);
+		roundDown.Clicked += (s, e) => CalculateTip (false, true);
+		roundUp.Clicked += (s, e) => CalculateTip(true, false);
+		tipPercentSlider.ValueChanged += (s, e) =>
+		{
+			double pct = Math.Round(e.NewValue);
+			tipPercent.Text = pct + "%";
+			CalculateTip(false, false);
+		};
+	}
+
+	void CalculateTip(bool roundUp, bool roundDown)
 	{
 		double t;
 		if (Double.TryParse(billInput.Text, out t) && t > 0)
@@ -21,11 +35,19 @@ public partial class MainPage : ContentPage
 			}
 			else if (roundDown)
 			{
-				final = Math.Ceiling(final);
+				final = Math.Floor(final);
 				tip = final - t;
 			}
-			tipOutput.Text = final.ToString("C");
+			tipOutput.Text = tip.ToString("C");
 			totalOutput.Text = final.ToString("C");
 		}
 	}
+	private void OnNormalTip(object sender, EventArgs e)
+	{
+		tipPercentSlider.Value = 15;
+	}
+    private void OnGenerousTip(object sender, EventArgs e)
+	{
+        tipPercentSlider.Value = 20;
+    }
 }
